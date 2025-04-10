afc11f4fa7f1a7d9919d89cc8a9c92c308222f24
just-hazard <edenhazard5870@gmail.com>
2025-04-10 15:59:21 +0900

    init

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+plugins {
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
+    kotlin ("plugin.allopen") version "1.9.25"
+    kotlin ("plugin.noarg") version "1.9.25"
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.task"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        val javaVersion = JavaVersion.VERSION_17.toString()
+        languageVersion.set(JavaLanguageVersion.of(javaVersion))
+    }
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+//    implementation("org.flywaydb:flyway-core")
+
+    runtimeOnly("com.h2database:h2")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("io.rest-assured:kotlin-extensions:5.1.1")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("io.mockk:mockk:1.14.0")
+
+    //utils
+    implementation("com.google.guava:guava:31.1-jre")
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/task/teamreboott/TeamReboottApplication.kt
@@ -0,0 +1,11 @@
+package com.task.teamreboott
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamReboottApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamReboottApplication>(*args)
+}
